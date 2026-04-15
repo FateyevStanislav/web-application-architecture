1ee64377ef4296c19cf7319df0a8b62a16ca2147
Fateyev Stanislav <fateev0803@gmail.com>
2026-04-16 02:24:53 +0300

    Lab9: REST API, CRUD comments, JS, React

--- src/boardy-api/main.py
+++ src/boardy-api/main.py
@@ -1,8 +1,23 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from datetime import datetime
 import aiomysql
+from routers import comments
  
 app = FastAPI(title='Boardy API', version='0.2.0')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://kilqa.ai-info.ru",          
+        "https://api.kilqa.ai-info.ru",      
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(comments.router)
  
 DB_CONFIG = {
     'host': '127.0.0.1',
